6786a28f728f79409674bfb4ee511b175a8af8ea
Serhii <grammas56@gmail.com>
2023-03-13 19:51:11 +0200

    text_mistake_removed

--- food/js/script.js
+++ food/js/script.js
@@ -9,7 +9,7 @@ import {openModal} from './modules/modal';
 
 window.addEventListener('DOMContentLoaded', function() {
     // Time changed of for maintanance.
-    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 5000000000);
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
     timer('.timer', '2023-06-11');
